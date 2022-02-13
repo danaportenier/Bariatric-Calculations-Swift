226050dfa27a1e035283715f8820855c9436d8af
Dana Portenier <dana.portenier@duke.edu>
2022-02-13 06:18:45 -0500

    Initial Commit

---
+++ Bariatric Calculations/Bariatric_CalculationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bariatric_CalculationsApp.swift
+//  Bariatric Calculations
+//
+//  Created by dana portenier on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Bariatric_CalculationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
